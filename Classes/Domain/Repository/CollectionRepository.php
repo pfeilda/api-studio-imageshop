774f6cc0d12c88bd9640ab0b2fa1e652bb786f88
pfeilda <daniel.pfeil@itpfeil.de>
2017-05-31 09:04:07 +0200

    implement controller

---
+++ Classes/Domain/Repository/CollectionRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Apistudio\Imageshop\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * Class Collection
+ * @package Apistudio\Imageshop\Domain\Repository
+ */
+class CollectionRepository extends Repository
+{
+
+}
---
+++ Classes/Domain/Repository/ProductRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Apistudio\Imageshop\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * Class Product
+ * @package Apistudio\Imageshop\Domain\Repository
+ */
+class ProductRepository extends Repository
+{
+
+}
